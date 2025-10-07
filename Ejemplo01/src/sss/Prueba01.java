95fb44dfbe495b65c6cbbd68f461787e57ee366c
oriol-valentines <oriolvalentinesmiro@gmail.com>
2025-10-07 17:49:27 +0200

    Merch r2

---
+++ Ejemplo01/src/sss/Prueba01.java
@@ -0,0 +1,14 @@
+package sss;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Prueba01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hola");
+	}
+
+}
